c0340802960e0342fcc6bf97e3148f8dd4ced403
tsukachu <concon.ch.1543@gmail.com>
2021-06-25 16:37:24 +0900

    main.Manager のテストを追加

--- tests/test_main.py
+++ tests/test_main.py
@@ -2,6 +2,8 @@ from runpy import run_module
 from unittest import TestCase
 from unittest.mock import patch
 
+from main import Manager
+
 
 class MainTests(TestCase):
     @patch("main.fire.Fire")
@@ -9,3 +11,20 @@ class MainTests(TestCase):
         run_module("main", run_name="__main__")
 
         mock_fire.assert_called()
+
+
+class ManagerTests(TestCase):
+    def setUp(self):
+        self.manager = Manager()
+
+    @patch("main.setup")
+    def test_call_of_setup(self, mock_setup):
+        self.manager.setup()
+
+        mock_setup.assert_called()
+
+    @patch("main.teardown")
+    def test_call_of_teardown(self, mock_teardown):
+        self.manager.teardown()
+
+        mock_teardown.assert_called()
