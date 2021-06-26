6453b7cfd18ec5bb08fb827d92a625b70c125813
tsukachu <concon.ch.1543@gmail.com>
2021-06-26 09:56:46 +0900

    settings の読み込みを修正

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 
 import fire
+from simple_settings import LazySettings
 
 from app.scraping.main import run
 from app.utils.test import setup, teardown
@@ -8,9 +9,13 @@ from app.utils.test import setup, teardown
 
 class Manager:
     def __init__(self, settings=None):
-        if not settings:
-            settings = "app.settings.development"
-        os.environ.setdefault("SIMPLE_SETTINGS", settings)
+        if not os.environ.get("SIMPLE_SETTINGS"):
+            if not settings:
+                settings = "app.settings.development"
+            os.environ.setdefault("SIMPLE_SETTINGS", settings)
+
+        settings = LazySettings(os.environ["SIMPLE_SETTINGS"])
+        settings.setup()
 
     def setup(self):
         setup()
