c2339f3bfe56ecc9cf3d1ec856afb09aa6787ce8
tsukachu <concon.ch.1543@gmail.com>
2021-06-28 22:01:55 +0900

    同じ放送日が既に登録されてる場合は version を更新して登録する

--- app/scraping/main.py
+++ app/scraping/main.py
@@ -1,6 +1,7 @@
 from logging import getLogger
 
 import requests
+from sqlalchemy import Date, cast
 
 from app.database import session
 from app.models import OnAirSchedule, OnAirScheduleSourceHistory
@@ -13,7 +14,9 @@ def run():
     page = OnAirSchedulePage(requests.Session())
     page.get()
     page.parse()
-    latest_source = OnAirScheduleSourceHistory.query.order_by(OnAirScheduleSourceHistory.id.desc()).first()
+    latest_source = OnAirScheduleSourceHistory.query.order_by(
+        OnAirScheduleSourceHistory.id.desc()
+    ).first()
     if latest_source and latest_source.source == page.contents.prettify():
         logger.debug("No change from latest history")
         return
@@ -25,11 +28,26 @@ def run():
             logger.debug(
                 f"{schedule['start']}, {schedule['end']}, {schedule['episode']}"
             )
+            latest_schedule = (
+                OnAirSchedule.query.filter(
+                    cast(OnAirSchedule.start, Date) == schedule["start"].date(),
+                    cast(OnAirSchedule.end, Date) == schedule["end"].date(),
+                )
+                .order_by(OnAirSchedule.version.desc())
+                .first()
+            )
+            if latest_schedule:
+                logger.debug("The Schedule for the same OnAir date already exists")
+                logger.debug("Increment the version and re-register")
+                version = latest_schedule.version + 1
+            else:
+                version = 1
             on_air = OnAirSchedule(
                 title=page.get_title(),
                 episode=schedule["episode"],
                 start=schedule["start"],
                 end=schedule["end"],
+                version=version,
             )
             session.add(on_air)
 
